ad07f15ade8aa5c49ca27021f786a4d95e24e196
JeffreyAkashLeenus <z949t764@wichita.edu>
2023-09-26 18:32:56 -0500

    Monkey Details View is created using monkeyList

---
+++ MonkeyFinder/Services/MonkeyService.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http.Json;
+
+namespace MonkeyFinder.Services;
+
+public class MonkeyService
+{
+    HttpClient httpClient;
+    public MonkeyService()
+    {
+        this.httpClient = new HttpClient();
+    }
+    List<Monkey> monkeyList = new();
+    public async Task<List<Monkey>> GetMonkeys()
+    {
+
+        using var stream = await FileSystem.OpenAppPackageFileAsync("monkeydata.json");
+        using var reader = new StreamReader(stream);
+        var contents = await reader.ReadToEndAsync();
+        monkeyList = JsonSerializer.Deserialize(contents, MonkeyContext.Default.ListMonkey);
+        return monkeyList;
+    }
+
+
+
+}
---
+++ MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MonkeyFinder.ViewModel;
+
+[QueryProperty(nameof(Monkey), "Monkey")]
+public partial class MonkeyDetailsViewModel : BaseViewModel
+{
+
+    [ObservableProperty]
+    Monkey monkey;
+}
