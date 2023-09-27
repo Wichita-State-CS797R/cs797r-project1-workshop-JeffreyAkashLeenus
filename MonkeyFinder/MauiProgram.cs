056125e99d525f603aad5ba2e4c23b1811db15a0
JeffreyAkashLeenus <z949t764@wichita.edu>
2023-09-26 22:25:06 -0500

    Part3: Monkey view and navigation is created for each monkey in list

--- MonkeyFinder/MauiProgram.cs
+++ MonkeyFinder/MauiProgram.cs
@@ -22,6 +22,8 @@ public static class MauiProgram
 		builder.Services.AddSingleton<MainPage>();
         builder.Services.AddSingleton<MonkeyService>();
         builder.Services.AddSingleton<MonkeysViewModel>();
+        builder.Services.AddTransient<MonkeyDetailsViewModel>();
+        builder.Services.AddTransient<DetailsPage>();
 
         return builder.Build();
 	}
