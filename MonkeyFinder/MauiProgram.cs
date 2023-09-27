96fece8f5a33511353b87a798f0c516a4017d940
JeffreyAkashLeenus <z949t764@wichita.edu>
2023-09-26 22:31:47 -0500

    Part4: Accessing Platform Feature and created closet monkey field

--- MonkeyFinder/MauiProgram.cs
+++ MonkeyFinder/MauiProgram.cs
@@ -24,6 +24,9 @@ public static class MauiProgram
         builder.Services.AddSingleton<MonkeysViewModel>();
         builder.Services.AddTransient<MonkeyDetailsViewModel>();
         builder.Services.AddTransient<DetailsPage>();
+        builder.Services.AddSingleton<IConnectivity>(Connectivity.Current);
+        builder.Services.AddSingleton<IGeolocation>(Geolocation.Default);
+        builder.Services.AddSingleton<IMap>(Map.Default);
 
         return builder.Build();
 	}
